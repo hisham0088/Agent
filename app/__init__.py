657572d6e5089d8513057b205b69570b2128b927
hisham0088 <hisham2005a@gmail.com>
2026-09-08 10:01:04 +0530

    Correct typos and set up Flask application
    Fix typos in function names and improve imports.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,19 @@
+import os, urllib.parse , urllib.request ,render_template
+from app.youtube import youtube_bp
+Gemini_api_Key = "Gemini_API_Key";
 
+def home():
+  return render_templete (" index.html")
+
+def create_app():
+  
+  app = Flask(_name_)
+
+app.register_blueprint(youtube_bp, url_prefix="/youtube")
+
+@app.route("/html")
+def html():
+
+    return render_templete("index.html")
+
+return app;
