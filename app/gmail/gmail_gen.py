e9fd591a7b953f9c824237555dfd0701078b4070
hisham0088 <hisham2005a@gmail.com>
2026-09-10 11:40:21 +0530

    Update gmail_gen.py

--- app/gmail/gmail_gen.py
+++ app/gmail/gmail_gen.py
@@ -25,3 +25,37 @@ Rules:
 -Keep the email natural and concise. 
 
 output exactly:
+
+SUBJECT<Subject>
+BODY:
+<email body>
+
+User command
+(command)
+"""
+
+  url=(
+    f"https://generativelanguage.googleapis.com/"
+    f"v1beta/models/(MODEL):generateContent"
+  )
+
+payload = {
+  "contents":[{"trext":prompt}}}];
+  "generationconig":{
+  "Temperature":0.7.
+  "maxOutputTockens":800
+  } 
+}
+
+req=urllib.request.Request(
+  url,
+  data=json.dumps(payload).encode(),
+  headers={
+    "content-type":"application json",
+    "x-goog-api-key":API_KEY
+  },
+  method="POST"
+)
+for attempt in range(4):
+  try:
+    with urllib.request.urlopen(req,timeout=30) as response:
