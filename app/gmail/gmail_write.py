7a86b71b518624aba411576ce539990237097617
hisham0088 <hisham2005a@gmail.com>
2026-09-11 11:23:57 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -2,36 +2,38 @@ import os
 import re
 import urllib.parse
 
-KEYWORD = (
-  "gmail","email","e-mail","mail"
-  "Write an email","send an email","draft an email",
-  "compose an email","write mail","send email","draft mail",
-  "compose mail"
+
+KEYWORDS = (
+    "gmail", "email", "e-mail", "mail",
+    "write an email", "send an email", "draft an email",
+    "compose an email", "write mail", "send mail", "draft mail",
+    "compose mail"
 )
 
-def is_mailcommand(text):
-  text =text.lower()
-  return any(k in text for k in KEYWORDS)
+def is_email_command(text):
+    text = text.lower()
+    return any(k in text for k in KEYWORDS)
 
 def extract_email(text):
-  match = re.search(r"[\w.=]+\.\w+",text)
-  if match:
-    return match.group(0)
+    match = re.search(r"[\w.+-]+@[\w.-]+\.\w+", text)
+    if match:
+        return match.group(0)
 
-match = re.search(
-  r"([/w.,+]+)\s+at\s+([\w.-]+)\s+dot\s+(\w+)",
-  text.lower()
-)
-if match:
-  return f"{match.group(1)}@{match.group(2)}.{match.group(3)}"
+    match = re.search(
+        r"([\w.+-]+)\s+at\s+([\w.-]+)\s+dot\s+(\w+)",
+        text.lower()
+    )
+    if match:
+        return f"{match.group(1)}@{match.group(2)}.{match.group(3)}"
 
-return**
-def create_gmail_url(subjects="",body="",recipients=""):
-  params = urllib.parase.urlencode({
-    "view":"cm",
-    "fs":"1",
-    "to":reciprint,
-    "SU":subject,
-    "body":body
-  })
-  return f"https://mail.google.com/mail/u/0/?(parms)"
+    return ""
+
+def create_gmail_url(subject="", body="", recipient=""):
+    params = urllib.parse.urlencode({
+        "view": "cm",
+        "fs": "1",
+        "to": recipient,
+        "su": subject,
+        "body": body
+    })
+    return f"https://mail.google.com/mail/u/0/?{params}"
